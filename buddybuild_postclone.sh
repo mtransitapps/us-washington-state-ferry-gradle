df9a14c8d36c2630c3069ac7f7329064efff33a6
Mathieu Méa <mathieu.mea@gmail.com>
2017-07-08 19:52:40 -0400

    Update git submodules.

---
+++ buddybuild_postclone.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "buddybuild post clone...";
+chmod +x gradlew;
+chmod +x build.sh;
+echo "buddybuild post clone... DONE";
---
+++ buddybuild_prebuild.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "buddybuild pre build...";
+./build.sh;
+echo "buddybuild pre build... DONE";
